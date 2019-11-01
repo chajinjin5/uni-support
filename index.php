065d4f9c91b466a6f198d2aace87dd5ac8a2d29d
chajinjin5 <57260123+chajinjin5@users.noreply.github.com>
2019-11-01 22:39:51 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="CSS_defalt.css">
+    <link rel="stylesheet" href="CSS_defalt1.css">
+    <link rel="stylesheet" href="CSS_style2.css">
+    <link rel="stylesheet" href="CSS_2.css">
+    <style>
+      li a.current { background-color: #FF6347; color: white; }
+
+      li a:hover:not(.current) { background-color: #CD853F; color: white; }
+    </style>
+  </head>
+  <body class="ab">
+    <h1>대학 모의 지원</h1>
+    <ul class="a">
+       <li><a href="main.php">Home</a></li>
+
+       <ul class="b">
+         <li><a id="c" href="login.php">로그인</a></li>
+         <li><a id="d"href="signup.html">회원가입</a></li>
+       </ul>
+
+    </ul>
+  </body>
+  <body >
+    <div class="d">
+      <ul class="e">
+        <li><label for="txt1">대학명</label><input type="C.N." id="txt1"
+          placeholder="대학명을 입력하십시오." required></li>
+
+        <li><label for="txt2">학과명</label><input type="D.P." id="txt2"
+          placeholder="학과명을 입력하십시오." required></li>
+        <li class="btn"><button type="submit">지원</button></li>
+      </ul>
+    </div>
+  </body>
+</html>
